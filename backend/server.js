31c064e2a64ea152f7f79d08c95d2537faf6eaaa
SyedMuhammadHaris <mohammadharisahmed.18@gmail.com>
2022-06-05 23:30:03 +0500

    REST API FOR GOAL DONE

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,19 @@ const dotenv = require('dotenv').config();
 const port = process.env.PORT || 8000;
 const app = express();
 const goalsRoute = require('./routes/goalRoutes');
-const { errorHandler}  = require('./middlewares/errorMiddleware')
+const { errorHandler}  = require('./middlewares/errorMiddleware');
+const connectDb = require('./config/db')
 
+connectDb();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+
+
 app.use('/api/goals',goalsRoute);
+
 app.use(errorHandler);
+
+
 app.listen(port,()=>{
     console.log(`Server started on port ${port}`);
 })
