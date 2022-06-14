7bb89d791b6c1893f26030ad83c8af012d813f9a
SyedMuhammadHaris <mohammadharisahmed.18@gmail.com>
2022-06-15 02:22:59 +0500

    Goals Functionality

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
+import goalreducer from '../features/goals/goalSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    goals:goalreducer
 },
 });
